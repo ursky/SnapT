de84e6f7871bdbc6652938a22ed06c8030fed8c3
Gherman Uritskiy <guritsk1@jhu.edu>
2019-02-08 17:18:20 -0500

    added second prodigal check

---
+++ bin/snapt_filter_by_prodigal.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import sys
+
+
+# load prodigal annotation of transcripts
+blacklist={}
+for line in open(sys.argv[1]):
+	if line[0]=="#":
+		continue
+	cut = line.strip().split("\t")
+	# important to look at only the relevant strand (the strand of the ncRNA)
+	if cut[6]!="+":
+		continue
+	transcript = cut[0].split("(")[0]
+	positions = transcript.split(":")[1].split("-")
+	rna_length = int(positions[1])-int(positions[0])
+	orf_length = int(cut[4]) - int(cut[3])
+	if orf_length > rna_length/3:
+		blacklist[transcript]=None
+
+# only print non-blacklisted transcripts
+for line in open(sys.argv[2]):
+	if line[0]=="#":
+		print line.strip()
+		continue
+	cut=line.strip().split("\t")
+	transcript=cut[0]+":"+str(int(cut[3])-1)+"-"+cut[4]
+	if transcript not in blacklist:
+		print line.strip()
+
+
+	
